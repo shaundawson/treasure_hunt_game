d62c6e333175bcef0e17c38e9f6498500b99268c
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-04-23 18:41:51 -0400

    Update notes.py
    odd or even exercise

--- notes.py
+++ notes.py
@@ -1,8 +1,17 @@
+# if...else Statement
+# print("Welcome to the rollercoaster!")
+# height = int(input("What is your height in cm? "))
 
-print("Welcome to the rollercoaster!")
-height = int(input("What is your height in cm? "))
+# if height >= 120: 
+#   print("You can ride the rollercoaster!")
+# else:
+#   print("Sorry, you have to grow taller before you can ride")
 
-if height > 120: 
-  print("You can ride the rollercoaster!")
-else:
-  print("Sorry, you have to grow taller before you can ride")
+
+# Odd or Even Exercise
+# Write a program that adds the digits in a 2 digit number. e.g. if the input was 35, then the output should be 3 + 5 = 8
+number = int(input("Which number do you want to check? "))
+if number % 2 == 0:
+  print("This is a even number.")
+else: 
+  print("This is an odd number.")
