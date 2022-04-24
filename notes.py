3b44c0a0e6d38b7163c110fc8f5e8d9f5a947719
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-04-24 09:55:52 -0400

    Pizza Order Program

--- notes.py
+++ notes.py
@@ -97,26 +97,54 @@
 
 
 # Multiple if Statements in Succession 
-print("Welcome to the rollercoaster!")
-height = int(input("What is your height in cm? "))
+# print("Welcome to the rollercoaster!")
+# height = int(input("What is your height in cm? "))
+# bill = 0
+
+# if height >= 120: 
+#   print("You can ride the rollercoaster!")
+#   age = int(input("What is your age?"))
+#   if age < 12:
+#      bill = 5 
+#      print("Child tickets are $5.")
+#   elif age <= 18:
+#     bill = 7
+#     print("Youth tickets are $7.")
+#   else:
+#     bill = 12
+#     print("Adult tickets are $12.")
+
+#   wants_photo = input("Do you want a photo taken? Y or N.")
+#   if wants_photo == "Y":
+#     bill += 3
+#   print(f"Your final bill is ${bill}")
+# else:
+#   print("Sorry, you have to grow taller before you can ride")
+
+
+# Pizza Order Practice
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
 bill = 0
 
-if height >= 120: 
-  print("You can ride the rollercoaster!")
-  age = int(input("What is your age?"))
-  if age < 12:
-     bill = 5 
-     print("Child tickets are $5.")
-  elif age <= 18:
-    bill = 7
-    print("Youth tickets are $7.")
-  else:
-    bill = 12
-    print("Adult tickets are $12.")
-
-  wants_photo = input("Do you want a photo taken? Y or N.")
-  if wants_photo == "Y":
+if size == "S" :
+  bill = 15
+  if add_pepperoni == "Y":
+    bill += 2
+    if extra_cheese == "Y":
+      bill +=1
+elif size == "M":
+  bill = 20
+  if add_pepperoni == "Y":
     bill += 3
-  print(f"Your final bill is ${bill}")
-else:
-  print("Sorry, you have to grow taller before you can ride")
+    if extra_cheese == "Y":
+      bill +=1
+elif size == "L":
+  bill = 25
+  if add_pepperoni == "Y":
+    bill += 3
+    if extra_cheese == "Y":
+      bill +=1
+print(f"Your final bill is ${bill}")
