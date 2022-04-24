0121639c3e21973e0e26369933cfed54bf77f5cc
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-04-23 21:05:12 -0400

    leap year checker - readable

--- notes.py
+++ notes.py
@@ -66,11 +66,23 @@
 
 # Leap Year Checker
 # Write a program that works out whether if a given year is a leap year.
-year = int(input("Which year do you want to check? "))
+# year = int(input("Which year do you want to check? "))
 
-if year % 4 == 0 and year % 100 != 0:
-  print("Leap year")
-elif year % 100 == 0 and year % 400 == 0:
-  print("Leap year")
+# if year % 4 == 0 and year % 100 != 0:
+#   print("Leap year")
+# elif year % 100 == 0 and year % 400 == 0:
+#   print("Leap year")
+# else:
+#   print("Not Leap Year")
+
+year = int(input("Which year do you want to check? "))
+if year % 4 == 0:
+  if year % 100 == 0:
+    if year % 400 == 0:
+      print("Leap year")
+    else:
+      print("Not leap year")
+  else:
+    print("Leap year")
 else:
-  print("Not Leap Year")
+  print("Not leap year")
