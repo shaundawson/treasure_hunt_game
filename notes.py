4ae9492070870107a74ec4a4ba272d6b1370ac50
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-04-24 10:09:01 -0400

    Pizza Order - simplify code

--- notes.py
+++ notes.py
@@ -129,18 +129,36 @@ add_pepperoni = input("Do you want pepperoni? Y or N ")
 extra_cheese = input("Do you want extra cheese? Y or N ")
 bill = 0
 
+# if size == "S" :
+#   bill = 15
+#   if add_pepperoni == "Y":
+#     bill += 2
+# elif size == "M":
+#   bill = 20
+#   if add_pepperoni == "Y":
+#     bill += 3
+# elif size == "L":
+#   bill = 25
+#   if add_pepperoni == "Y":
+#     bill += 3
+# if extra_cheese == "Y":
+#   bill += 1
+# print(f"Your final bill is: ${bill}")
+
 if size == "S" :
-  bill = 15
-  if add_pepperoni == "Y":
-    bill += 2
+  bill += 15
 elif size == "M":
-  bill = 20
-  if add_pepperoni == "Y":
-    bill += 3
+  bill += 20
 elif size == "L":
-  bill = 25
-  if add_pepperoni == "Y":
+  bill += 25
+
+if add_pepperoni == "Y":
+  if size == "S":
+    bill += 2
+  else:
     bill += 3
+
 if extra_cheese == "Y":
   bill += 1
+
 print(f"Your final bill is: ${bill}")
