7a857ffffccd566bf5c1a868e29cbd58b080c13e
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-04-24 10:01:12 -0400

    Pizza Order - fix extra_cheese calc

--- notes.py
+++ notes.py
@@ -133,18 +133,14 @@ if size == "S" :
   bill = 15
   if add_pepperoni == "Y":
     bill += 2
-    if extra_cheese == "Y":
-      bill +=1
 elif size == "M":
   bill = 20
   if add_pepperoni == "Y":
     bill += 3
-    if extra_cheese == "Y":
-      bill +=1
 elif size == "L":
   bill = 25
   if add_pepperoni == "Y":
     bill += 3
-    if extra_cheese == "Y":
-      bill +=1
-print(f"Your final bill is ${bill}")
+if extra_cheese == "Y":
+  bill += 1
+print(f"Your final bill is: ${bill}")
