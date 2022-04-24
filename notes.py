dd79481e6fdc13d1c2408d3cfdd9d42b2c1ee6d8
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-04-23 21:22:36 -0400

    Multiple if Statements in Succession

--- notes.py
+++ notes.py
@@ -8,6 +8,7 @@
 #   print("Sorry, you have to grow taller before you can ride")
 
 
+
 # Odd or Even Exercise
 # Write a program that adds the digits in a 2 digit number. e.g. if the input was 35, then the output should be 3 + 5 = 8
 # number = int(input("Which number do you want to check? "))
@@ -15,6 +16,9 @@
 #   print("This is a even number.")
 # else: 
 #   print("This is an odd number.")
+
+
+
 
 # Nested if/else
 # print("Welcome to the rollercoaster!")
@@ -46,6 +50,8 @@
 # else:
 #   print("Sorry, you have to grow taller before you can ride")
 
+
+
 # BMI Calculator
 # Write a program that interprets the Body Mass Index (BMI) based on a user's weight and height.
 # height = float(input("enter your height in m: "))
@@ -64,6 +70,8 @@
 # else:
 #   print(f"Your BMI is {bmi}, you are clinically obese.")
 
+
+
 # Leap Year Checker
 # Write a program that works out whether if a given year is a leap year.
 # year = int(input("Which year do you want to check? "))
@@ -75,14 +83,40 @@
 # else:
 #   print("Not Leap Year")
 
-year = int(input("Which year do you want to check? "))
-if year % 4 == 0:
-  if year % 100 == 0:
-    if year % 400 == 0:
-      print("Leap year")
-    else:
-      print("Not leap year")
+# year = int(input("Which year do you want to check? "))
+# if year % 4 == 0:
+#   if year % 100 == 0:
+#     if year % 400 == 0:
+#       print("Leap year")
+#     else:
+#       print("Not leap year")
+#   else:
+#     print("Leap year")
+# else:
+#   print("Not leap year")
+
+
+# Multiple if Statements in Succession 
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+bill = 0
+
+if height >= 120: 
+  print("You can ride the rollercoaster!")
+  age = int(input("What is your age?"))
+  if age < 12:
+     bill = 5 
+     print("Child tickets are $5.")
+  elif age <= 18:
+    bill = 7
+    print("Youth tickets are $7.")
   else:
-    print("Leap year")
+    bill = 12
+    print("Adult tickets are $12.")
+
+  wants_photo = input("Do you want a photo taken? Y or N.")
+  if wants_photo == "Y":
+    bill += 3
+  print(f"Your final bill is ${bill}")
 else:
-  print("Not leap year")
+  print("Sorry, you have to grow taller before you can ride")
